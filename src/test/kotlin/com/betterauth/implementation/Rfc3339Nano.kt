e9cbff7636b9df047b3c637f14f6c310b54b314b
Jason Colburne <j.colburne@gmail.com>
2025-10-09 15:23:49 -0300

    timestamps

--- src/test/kotlin/com/betterauth/implementation/Rfc3339Nano.kt
+++ src/test/kotlin/com/betterauth/implementation/Rfc3339Nano.kt
@@ -7,14 +7,14 @@ import java.util.TimeZone
 
 class Rfc3339Nano : Timestamper {
     private val isoFormat =
-        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").apply {
+        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").apply {
             timeZone = TimeZone.getTimeZone("UTC")
         }
 
     override fun format(date: Date): String {
         val isoString = isoFormat.format(date)
-        val millis = String.format("%03d", date.time % 1000)
-        return "$isoString.${millis}000000Z"
+        // Extend milliseconds (3 digits) to nanoseconds (9 digits) by appending 6 zeros
+        return "${isoString}000000Z"
     }
 
     override fun parse(dateString: String): Date = Date.parse(dateString).let { Date(it) }
